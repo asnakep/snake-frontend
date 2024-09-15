4fad764e3aaea0b7432e4ba98b805914f68b811d
Manuel <econcotelli@proton.me>
2024-09-15 17:11:52 +0000

    made template for sharing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,13 +6,12 @@ export default function Footer() {
     <footer className="footer footer-center p-10">
       <div>
         <p className="font-bold">
-          SN₳KE Stake Pool <br />
+          NAME Stake Pool <br />
         </p>
-        <p>Copyright © 2024 - All right reserved</p>
       </div>
       <div>
         <div className="grid grid-flow-col gap-4">
-          <Link href="https://twitter.com/adasnakepool">
+          <Link href="https://twitter.com">
             <Image
               className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] "
               src="logo-x.svg"
